5634e1e845992a899b1fc65ee10f53890f562f8b
s24586 <s24586@pjwstk.edu.pl>
2023-06-24 12:39:31 +0200

    Dodanie projektu

---
+++ src/main/java/com/example/demo/JazS24586NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS24586NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS24586NbpApplication.class, args);
+	}
+
+}
